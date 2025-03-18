f76e24b81d082fd2858f0f3e398414196d342270
Ulug'bek <140692404+UlugbekKimsanov@users.noreply.github.com>
2025-03-18 17:21:44 +0500

    Add files via upload

---
+++ LeedCode/src/test/java/com/example/leedcode/LeedCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leedcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeedCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
