002cfff0b7d056b8e64f4f3a6b6062b7f604ad29
Ulug'bek <kimsanov0717@gmail.com>
2025-08-02 19:16:21 +0500

    LeedCode some problems solved.

--- LeedCode/src/main/java/leedcode/problems/DataBase.java
+++ LeedCode/src/main/java/leedcode/problems/DataBase.java
@@ -2,13 +2,13 @@ package leedcode.problems;
 
 public class DataBase {
 
-    public void LeedCode2356(){
+    public void LeedCode2356() {
         String mySqlQuery = """
-        SELECT
-        teacher_id,
-                COUNT(DISTINCT subject_id) AS cnt
-        FROM
-                Teacher
-        GROUP BY teacher_id """;
+                SELECT
+                teacher_id,
+                        COUNT(DISTINCT subject_id) AS cnt
+                FROM
+                        Teacher
+                GROUP BY teacher_id """;
     }
 }
