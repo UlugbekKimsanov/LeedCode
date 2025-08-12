a2bbf4278c30e506916185fa5344a65463abb106
Ulug'bek <kimsanov0717@gmail.com>
2025-08-12 12:26:56 +0500

    LeedCode203 #LinkedList removeElements solved.

--- LeedCode/src/main/java/leedcode/problems/LinkedList.java
+++ LeedCode/src/main/java/leedcode/problems/LinkedList.java
@@ -50,6 +50,24 @@ public class LinkedList {
         return a;
     }
 
+    public ListNode removeElements203(ListNode head, int val) {
+
+        ListNode dummy = new ListNode(0);
+
+        dummy.next = head;
+
+        ListNode current = dummy;
+
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            }else {
+                current = current.next;
+            }
+        }
+        return dummy.next;
+    }
+
 
 
     public ListNode deleteDuplicates83(ListNode head) {
